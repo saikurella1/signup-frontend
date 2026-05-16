07fb04aa4b76c9610c96b037fb38451bcb9ba62b
Sai Teja <krishcary1@outlook.com>
2026-05-17 00:27:59 +0530

    Initial frontend commit

---
+++ signup.js
@@ -0,0 +1,52 @@
+
+async function signup(){
+        let firstName = document.getElementById("firstname").value.trim();
+        let lastName = document.getElementById("lastname").value.trim();
+        let emailid = document.getElementById("emailid").value.trim();
+        let mobileNumber = document.getElementById("mobilenumber").value.trim();
+    
+        let password = document.getElementById("password").value.trim();
+
+            if (
+                firstName === "" ||
+                lastName === "" ||
+                emailid === "" ||
+                mobileNumber === "" ||
+                password === "" 
+            ) {
+        
+                document.getElementById("result").textContent =
+                    "All fields are mandatory";
+        
+                return;
+            }
+            let response = await fetch(
+                "https://signup-project-1-czli.onrender.com/signup",
+                {
+                    method: "POST",
+        
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+        
+                    body: JSON.stringify({
+                        firstName: firstName,
+                        lastName: lastName,
+                        emailid: emailid,
+                        mobileNumber: mobileNumber,
+                        password: password,
+                    })
+                }
+            );
+            let result = await response.text();
+
+    document.getElementById("result").textContent = result;
+        
+
+    }
+
+   
+
+        
+        
+    
